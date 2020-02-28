917c2f2a26daac8c00f7a71ab779482d90d3fb4e
Esquinazi <esquinazi.dani@gmail.com>
2020-02-28 14:48:26 -0300

    Remove slug from md files

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,17 +1,4 @@
-const { createFilePath } = require(`gatsby-source-filesystem`);
 const path = require('path');
-
-exports.onCreateNode = ({ node, getNode, actions }) => {
-  const { createNodeField } = actions;
-  if (node.internal.type === `MarkdownRemark`) {
-    const slug = createFilePath({ node, getNode, basePath: `pages` });
-    createNodeField({
-      node,
-      name: `slug`,
-      value: slug,
-    });
-  }
-};
 
 exports.createPages = ({ actions, graphql }) => {
   const { createPage } = actions;
